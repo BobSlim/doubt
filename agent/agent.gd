6386f2763ce5c8a38f51feaa677c92a3666efc84
Robert Lim <robertslim42@gmail.com>
2024-01-15 19:50:56 +1100

    uses flip_h property

--- agent/agent.gd
+++ agent/agent.gd
@@ -7,12 +7,11 @@ const JUMP_VELOCITY = -180.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var can_double_jump = false
 
-enum FACING {
-	LEFT,
-	RIGHT,
-}
+func _ready():
+	pass
 
-var facing = FACING.RIGHT
+func _process(delta):
+	pass
 
 func _physics_process(delta):
 	velocity.y += gravity * delta
@@ -28,9 +27,10 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
+	var is_right = true if direction >= 0 else false
 	if direction:
 		velocity.x = direction * SPEED
-		$sprite.scale = Vector2(direction, 1)
+		$sprite.flip_h = !is_right
 		$sprite.play("walk")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
