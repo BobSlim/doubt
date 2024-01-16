eda6b8b4fcac3d855667ff7d98a7b8ebcc0e7461
Robert Lim <robertslim42@gmail.com>
2024-01-16 22:39:49 +1100

    adds doubt enums and exports calculation logic to subsystem

--- doubt/doubtTimer.gd
+++ doubt/doubtTimer.gd
@@ -5,15 +5,14 @@ extends Control
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 	
-
-func _on_doubt_doubt_changed(value):
-	var integral = int(value)
-	var fractional = value - integral
-	$barMain.value = fractional
+func _on_doubt_doubt_changed(doubt):
+	var integral = int(doubt)
+	var fractional = doubt - integral
+	# this allows for the eight section bar.
+	$barMain.value = ceil(fractional * 8)
 	$bar1.value = 1 if integral >= 1 else 0
 	$bar2.value = 1 if integral >= 2 else 0
