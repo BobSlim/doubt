324f4b4aaf8109186feaee12dc963ff858776904
Robert Lim <robertslim42@gmail.com>
2024-01-17 23:19:30 +1100

    adds parallax background

--- entity/walker/walker.gd
+++ entity/walker/walker.gd
@@ -21,7 +21,6 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var is_right = true if direction >= 0 else false
@@ -34,5 +33,3 @@ func _physics_process(delta):
 		sprite.play("default")
 
 	move_and_slide()
-
-
